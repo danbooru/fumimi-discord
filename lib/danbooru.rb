d39d146d3c8153f8db32a1fdd88202fa30427bbc
evazion <noizave@gmail.com>
2017-05-27 09:07:57 -0500

    Danbooru: add bans resource.

--- lib/danbooru.rb
+++ lib/danbooru.rb
@@ -9,7 +9,7 @@ require "danbooru/wiki"
 
 class Danbooru
   attr_reader :host, :user, :api_key, :site
-  attr_reader :posts, :users, :comments, :forum_posts, :wiki, :tags
+  attr_reader :posts, :users, :comments, :forum_posts, :wiki, :tags, :bans
 
   def initialize(host: ENV["BOORU_HOST"], user: ENV["BOORU_USER"], api_key: ENV["BOORU_API_KEY"])
     @host, @user, @api_key = host, user, api_key
@@ -22,10 +22,11 @@ class Danbooru
 
     @posts = @site["/posts"]
     @users = @site["/users"]
-    @comments = @site["/comments"].with(group_by: :comment, "search[order]": :id_desc)
+    @comments = @site["/comments"].with(group_by: :comment)
     @forum_posts = @site["/forum_posts"]
     @wiki = @site["/wiki_pages"]
     @tags = @site["/tags"]
+    @bans = @site["/bans"]
 
     posts.factory = Danbooru::Post
     comments.factory = Danbooru::Comment
