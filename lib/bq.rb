91af27abc951a98b8a89bf35b187122408da9a71
evazion <noizave@gmail.com>
2017-10-29 20:37:10 -0500

    BQ: refactor resolve_user_ids!; refactor `query` to take params.

--- lib/bq.rb
+++ lib/bq.rb
@@ -4,49 +4,28 @@ require "terminal-table"
 class BQ
   class BigQueryError < StandardError; end
 
-  attr_reader :bq, :project, :dataset, :booru, :timeout, :max
+  attr_reader :bq, :project, :dataset, :booru, :timeout
 
-  def initialize(project:, dataset:, booru:, timeout: 60000, max: 50)
-    @project, @dataset, @booru, @timeout, @max = project, dataset, booru, timeout, max
+  def initialize(project:, dataset:, booru:, timeout: 60000)
+    @project, @dataset, @booru, @timeout = project, dataset, booru, timeout
     @bq = Google::Cloud::Bigquery.new
   end
 
-  def exec(query)
-    results = bq.query(query, project: project, dataset: dataset, standard_sql: true, timeout: timeout, max: max)
+  def exec(query, **params)
+    results = bq.query(query, project: project, dataset: dataset, standard_sql: true, timeout: timeout, **params)
     raise BigQueryError if results.job.failed?
 
     results
   end
 
-  def query(query)
-    results = exec(query)
-    results.extend(Formatter)
-    resolve_user_ids!(results)
+  def query(query, **params)
+    results = exec(query, **params)
+    results.extend(BQMethods)
 
     results
   end
 
-protected
-
-  def resolve_user_ids!(results)
-    id_field = /(updater|creator|user)_id/
-
-    id_fields = results.headers.grep(id_field)
-    user_ids = results.map { |row| row.values_at(*id_fields) }.flatten.compact.sort.uniq
-    users = booru.users.search(id: user_ids.join(","))
-
-    results.map! do |row|
-      row.map do |k, v|
-        if k =~ id_field
-          [$1.to_sym, users.find { |user| user.id == v }.try(:name) || "MD Anonymous"]
-        else
-          [k, v]
-        end
-      end.to_h
-    end
-  end
-
-  module Formatter
+  module BQMethods
     def to_table(title="")
       rows = map(&:values)
 
@@ -63,6 +42,26 @@ protected
       footer = "#{table.rows.size} of #{total} rows | #{(job.ended_at - job.started_at).round(3)} seconds | #{total_bytes.to_s(:human_size)} (cached: #{cache_hit?})"
 
       "```\n#{title}\n#{table}\n#{footer}```"
+    end
+
+    def resolve_user_ids!(booru)
+      id_field = /(updater|creator|user|uploader)_id/
+
+      id_fields = headers.grep(id_field)
+      user_ids = map { |row| row.values_at(*id_fields) }.flatten.compact.sort.uniq
+      users = booru.users.search(id: user_ids.join(","))
+
+      map! do |row|
+        row.map do |k, v|
+          if k =~ id_field
+            [$1.to_sym, users.find { |user| user.id == v }.try(:name) || "MD Anonymous"]
+          else
+            [k, v]
+          end
+        end.to_h
+      end
+
+      self
     end
   end
 end
