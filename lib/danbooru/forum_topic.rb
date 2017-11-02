5c0847bd02bfaa50de697c13c1a917bc08537ecb
evazion <noizave@gmail.com>
2017-11-02 14:53:47 -0500

    Add pool, forum topic models.

---
+++ lib/danbooru/forum_topic.rb
@@ -0,0 +1,6 @@
+require "danbooru/model"
+
+class Danbooru
+  class ForumTopic < Danbooru::Model
+  end
+end
---
+++ lib/danbooru/pool.rb
@@ -0,0 +1,6 @@
+require "danbooru/model"
+
+class Danbooru
+  class Pool < Danbooru::Model
+  end
+end
