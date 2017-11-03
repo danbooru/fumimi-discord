76e1bfbc3e2850d205f8a054b5ec7953bba909d2
evazion <noizave@gmail.com>
2017-11-02 21:15:17 -0500

    Time#to_pretty: print zero seconds as "just now".

--- lib/fumimi/util.rb
+++ lib/fumimi/util.rb
@@ -3,7 +3,8 @@ module Fumimi::PrettyDate
     seconds = (Time.now - self).to_i
 
     case seconds
-      when 0..59 then "#{seconds}s ago" 
+      when 0 then "just now"
+      when 1..59 then "#{seconds}s ago" 
       when 60..3540 then "#{seconds/60}m ago"
       when 3541..82800 then "#{((seconds+99)/3600)}h ago"
       when 82801..518400 then "#{((seconds+800)/(60*60*24))}d ago"
