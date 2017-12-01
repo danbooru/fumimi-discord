284b351b8b6cb57a367da547f403fae75011d23f
evazion <noizave@gmail.com>
2017-12-01 17:07:30 -0600

    Danbooru::Model: generalize deserialization of timestamps.

--- lib/danbooru/model.rb
+++ lib/danbooru/model.rb
@@ -12,7 +12,7 @@ class Danbooru
       attributes = attributes.map do |attr, value|
         value =
           case attr
-          when "created_at", "updated_at", "last_commented_at", "last_comment_bumped_at", "last_noted_at"
+          when /_at$/
             Time.parse(value) rescue nil
           when /_url$/
             Addressable::URI.parse(value)
