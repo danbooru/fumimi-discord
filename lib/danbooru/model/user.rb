88508d10865aacff1ab4236ffd565408f32b95fd
evazion <noizave@gmail.com>
2017-12-02 14:32:29 -0600

    Danbooru::Model::User#url: remove (use default impl.)

--- lib/danbooru/model/user.rb
+++ lib/danbooru/model/user.rb
@@ -1,10 +1,6 @@
 require "danbooru/model"
 
 class Danbooru::Model::User < Danbooru::Model
-  def url
-    "#{booru.host}/users?name=#{CGI::escape(name)}"
-  end
-
   def at_name
     "@#{name}"
   end
