b4c588e865dabb65e3c8401d6602331ef9d069f0
evazion <noizave@gmail.com>
2017-12-01 18:04:09 -0600

    Danbooru: add remaining api endpoints.

--- lib/danbooru.rb
+++ lib/danbooru.rb
@@ -5,12 +5,17 @@ Dir[__dir__ + "/danbooru/**/*.rb"].each { |file| require file }
 
 class Danbooru
   RESOURCES = %i[
-    bans comments iqdb_queries forum_posts forum_topics pools posts source tags users wiki_pages
+    artist_commentaries artist_commentary_versions artists artist_versions bans
+    bulk_update_requests comments comment_votes counts delayed_jobs dmails
+    dtext_previews favorite_groups favorites forum_posts forum_topics ip_bans
+    iqdb_queries mod_actions notes note_previews note_versions pools
+    pool_versions posts post_appeals post_flags post_replacements post_versions
+    post_votes related_tags saved_searches source tag_aliases tag_implications
+    tags uploads users user_feedbacks wiki_pages wiki_page_versions
   ]
 
   attr_reader :host, :user, :api_key, :site
   attr_reader *RESOURCES.map(&:to_sym) # attr_reader :bans, :comments, ...
-  attr_reader :count
 
   def initialize(host: ENV["BOORU_HOST"], user: ENV["BOORU_USER"], api_key: ENV["BOORU_API_KEY"], factory: {})
     @user, @api_key = user, api_key
