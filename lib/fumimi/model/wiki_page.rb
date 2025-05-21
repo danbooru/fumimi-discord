3c6a78637ee428090006e2a4cc6d08143aca33c6
nonamethanks <hellafrickingepic@gmail.com>
2025-05-22 01:51:29 +0200

    Fix [[39]] returning [[fundoshi]]

--- lib/fumimi/model/wiki_page.rb
+++ lib/fumimi/model/wiki_page.rb
@@ -4,23 +4,21 @@ class Fumimi::Model::WikiPage < Fumimi::Model
   include Fumimi::HasDTextFields
 
   def self.render_wiki_page(channel, title, booru)
-    title = title.tr(" ", "_")
+    wiki_page = booru.wiki_pages.search(title_normalize: title).first
+    tag = wiki_page&.tag
 
-    wiki_page = booru.wiki_pages.show(CGI.escape(title))
-    tag = wiki_page.tag rescue nil
-
-    post = tag.example_post if tag&.post_count.to_i.positive?
-    channel.send_embed { |embed| embed(embed, channel, title, wiki_page, post) }
+    post = tag&.example_post if tag&.post_count.to_i.positive?
+    channel.send_embed { |embed| embed(embed, channel, booru, title, wiki_page, post) }
   end
 
-  def self.embed(embed, channel, title, wiki_page = nil, post = nil)
+  def self.embed(embed, channel, booru, title, wiki_page = nil, post = nil)
     embed.title = title.tr("_", " ")
 
-    if wiki_page.succeeded?
+    if wiki_page.present?
       embed.url = wiki_page.try(:url)
       embed.description = wiki_page.try(:pretty_body)
     else
-      embed.url = "#{wiki_page.booru.url}/posts?tags=#{CGI.escape(title)}"
+      embed.url = "#{booru.url}/posts?tags=#{CGI.escape(title)}"
       embed.description = "There is currently no wiki page for the tag `#{title}`."
     end
 
