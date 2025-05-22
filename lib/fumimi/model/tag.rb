e41aca14003dd8267cb7bf142e4af468f72d8a56
nonamethanks <hellafrickingepic@gmail.com>
2025-05-22 09:57:10 +0200

    tag extraction: simplify code a bit

--- lib/fumimi/model/tag.rb
+++ lib/fumimi/model/tag.rb
@@ -5,54 +5,70 @@ class Fumimi::Model::Tag < Fumimi::Model
     tags = booru.tags.search(name_or_alias_matches: title)
     tag = tags.max_by(&:post_count) || tags.first
 
-    channel.send_embed { |embed| embed(embed, channel, booru, title, tag) }
+    channel.send_embed { |embed| embed(embed, channel, title, tag) }
   end
 
-  def self.embed(embed, channel, booru, title, tag)
+  def self.embed(embed, channel, title, tag)
     embed.description = ""
     embed.description << "-# Aliased from `#{title}`.\n\n" if tag&.resolved_name != title.tr(" ", "_")
 
-    title = tag&.resolved_name || title
-    embed.title = title.tr("_", " ")
+    embed.title = (tag&.resolved_name || title).tr("_", " ")
+    embed.url = tag&.embed_url
 
-    if tag.try(:wiki_page).present?
-      embed.url = tag&.wiki_page.try(:url)
-      embed.description << tag&.wiki_page.try(:pretty_body)
-    else
-      embed.url = "#{booru.url}/posts?tags=#{CGI.escape(title)}"
-      embed.description << "There is currently no wiki page for the tag `#{title}`."
-    end
+    embed.description << tag&.wiki_preview
 
     post = tag&.example_post
-    return embed unless post
+    embed.image = post.embed_image(channel.name) if post.present?
 
-    embed.image = post.embed_image(channel.name)
-
-    embed.author = Discordrb::Webhooks::EmbedAuthor.new(
-      name: post.shortlink,
-      url: post.url
-    )
+    embed.author = tag&.embed_author
   end
 
   def resolved_name
     try(:antecedent_alias).try(:consequent_name) || name
   end
 
+  def embed_url
+    if try(:wiki_page).present?
+      "#{api.booru.url}/wiki_pages/#{wiki_page.id}"
+    else
+      "#{api.booru.url}/posts?tags=#{CGI.escape(resolved_name)}"
+    end
+  end
+
+  def wiki_preview
+    if try(:wiki_page).present?
+      wiki_page.try(:pretty_body)
+    else
+      "There is currently no wiki page for the tag `#{resolved_name}`."
+    end
+  end
+
+  def embed_author
+    return unless example_post
+
+    Discordrb::Webhooks::EmbedAuthor.new(
+      name: example_post.shortlink,
+      url: example_post.url
+    )
+  end
+
   def example_post
     return if post_count.to_i.zero?
 
-    search = case category
-             when 1 # artist
-               "#{name} rating:general order:score filetype:jpg limit:1 status:any"
-             when 3 # copy
-               "#{name} everyone rating:general order:score filetype:jpg limit:1 status:any copytags:<5 -parody -crossover"
-             when 4 # char
-               "#{name} solo chartags:<5 rating:general order:score filetype:jpg limit:1 status:any"
-             else # meta or general
-               "#{name} rating:general -animated -6+girls -comic order:score limit:1 status:any"
-             end
+    case category
+    when 1 # artist
+      search = "#{name} rating:general order:score filetype:jpg limit:1 status:any"
+    when 3 # copy
+      search = "#{name} everyone rating:general order:score filetype:jpg limit:1 status:any copytags:<5 -parody -crossover"
+    when 4 # char
+      search = "#{name} solo chartags:<5 rating:general order:score filetype:jpg limit:1 status:any"
+    else # meta or general
+      search = "#{name} rating:general -animated -6+girls -comic order:score limit:1 status:any"
+    end
 
-    response = booru.posts.index(tags: search)
-    response.first unless response.failed?
+    @example_post ||= begin
+      response = booru.posts.index(tags: search)
+      response.first unless response.failed?
+    end
   end
 end
