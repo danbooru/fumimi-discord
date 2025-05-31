2b72e4cac305d9e7c41090a3ed17b46cbffe7da0
nonamethanks <hellafrickingepic@gmail.com>
2025-05-31 16:44:57 +0200

    Factor out generate_column_widths

--- lib/fumimi/post_report.rb
+++ lib/fumimi/post_report.rb
@@ -37,34 +37,28 @@ class Fumimi::PostReport
   end
 
   def generate_table(headers:, rows:)
-    column_lengths = []
-
-    headers.each_with_index do |header, index|
-      column = rows.map { |l| l[index] }.map(&:to_s)
-      max_column_length = column.max_by(&:length).length
-      column_lengths << [header.length, max_column_length].max
-    end
+    column_widths = generate_column_widths(headers, rows)
 
     table = "```\n"
     horizontal_separator = ""
 
     headers.each_with_index do |_header, index|
-      column_length = column_lengths[index]
-      horizontal_separator << "+-#{"-" * column_length}-"
+      column_width = column_widths[index]
+      horizontal_separator << "+-#{"-" * column_width}-"
     end
     table << "#{horizontal_separator}+\n"
 
     headers.each_with_index do |header, index|
-      column_length = column_lengths[index]
-      table << "| #{header.ljust(column_length)} "
+      column_width = column_widths[index]
+      table << "| #{header.ljust(column_width)} "
     end
     table << "|\n"
     table << "#{horizontal_separator}+\n"
 
     rows.each do |row|
       row.each_with_index do |value, index|
-        column_length = column_lengths[index]
-        table << "| #{value.to_s.ljust(column_length)} "
+        column_width = column_widths[index]
+        table << "| #{value.to_s.ljust(column_width)} "
       end
       table << "|\n"
     end
@@ -73,5 +67,14 @@ class Fumimi::PostReport
     table << "```"
 
     table
+  end
+
+  def generate_column_widths(headers, rows)
+    # returns a list of widths for supplied headers + rows, for table autospacing
+    headers.each_with_index.map do |header, index|
+      column = rows.map { |l| l[index] }.map(&:to_s)
+      max_column_width = column.max_by(&:length).length
+      [header.length, max_column_width].max
+    end
   end
 end
