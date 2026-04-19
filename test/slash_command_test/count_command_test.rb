5687db4765e9cefef183427dbe9873bb5b892e0f
nonamethanks <hellafrickingepic@gmail.com>
2026-04-19 19:22:56 +0200

    Add /count tests

---
+++ test/slash_command_test/count_command_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class CountCommandTest < Minitest::Test
+  include TestMocks
+
+  def test_find_results
+    mock_slash_command("/count", args: {tags: "age:<1d"}) => { replies:, messages:, ** }
+
+    assert_match /Post count for `age:<1d`: [\d,]+./, replies.first
+  end
+end
