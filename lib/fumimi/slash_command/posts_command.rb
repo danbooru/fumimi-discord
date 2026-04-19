d6ee7d7317a1c27266ad5d6db895de5d3a17aaa8
nonamethanks <hellafrickingepic@gmail.com>
2026-04-19 18:57:03 +0200

    Add /posts command

---
+++ lib/fumimi/slash_command/posts_command.rb
@@ -0,0 +1,23 @@
+require "fumimi/slash_command"
+
+class Fumimi::SlashCommand::PostsCommand < Fumimi::SlashCommand
+  def self.name
+    "posts"
+  end
+
+  def self.description
+    "Do a post search."
+  end
+
+  def self.options
+    [
+      { type: OPTION_TYPES[:string], name: "tags", description: "Tags for the post.", required: false },
+      { type: OPTION_TYPES[:integer], name: "limit", description: "Max amount to return.", required: false, min_value: 1, max_value: 10 }, # rubocop:disable Layout/LineLength
+    ]
+  end
+
+  def embeds
+    posts = @booru.posts.index(tags: arguments[:tags], limit:arguments[:limit] || 3)
+    posts.map { |post| post.embed(nsfw_channel: @event.channel.nsfw?) }
+  end
+end
