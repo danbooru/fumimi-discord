danbooru/fumimi-discord

commit ab98fbf7d5bf6eefd02ae2780c10f11433c135cd

Fix error when bot doesn't have `server_messages` intent.
Fix this error:

    You attempted to identify with privileged intents that your bot is not authorized to use
    Please enable the privileged intents on the bot page of your application on the discord developer page.

This occurs when the bot doesn't have the "Message Content Intent" privilege enabled.

lib/fumimi.rb
@@ -51,7 +51,8 @@ class Fumimi
       name: "Robot Maid Fumimi",
       client_id: client_id,
       token: token,
-      prefix: "/"
+      prefix: "/",
+      intents: [:server_messages]
     )
   end
 

commit 133349d331d1f52665c722c85f6775c6c5944d6a

Remove intent from bot init
Regression from ab98fbf7d5bf6eefd02ae2780c10f11433c135cd

lib/fumimi.rb
@@ -51,8 +51,7 @@ class Fumimi
       name: "Robot Maid Fumimi",
       client_id: client_id,
       token: token,
-      prefix: "/",
-      intents: [:server_messages]
+      prefix: "/"
     )
   end
 