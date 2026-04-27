cc37f4d83c2d4fa7630cd4c74f49497905a62b9e
nonamethanks <hellafrickingepic@gmail.com>
2026-04-27 17:46:55 +0200

    Add count timeout tests

--- test/slash_command_test/count_command_test.rb
+++ test/slash_command_test/count_command_test.rb
@@ -6,4 +6,14 @@ class CountCommandTest < ApplicationTest
 
     assert_match(/Post count for `age:<1d`: [\d,]+./, replies.first)
   end
+
+  def test_timeout
+    mock_slash_command("/count", args: { tags: "-has:commentary commentary_request" }) => { reply_embeds:, replies: }
+
+    assert_equal [], replies
+    assert_equal 1, reply_embeds.length
+
+    error = reply_embeds.first
+    assert_equal "Timeout Encontered!", error.title
+  end
 end
